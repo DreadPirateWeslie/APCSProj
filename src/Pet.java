ac6443eaf7a2c53592c378eefce9109fa20a0d00
DreadPirateWeslie <grantfinnegan@gmail.com>
2017-03-08 13:09:07 -0600

    howdy

--- src/Pet.java
+++ src/Pet.java
@@ -1,28 +1,81 @@
+import sun.java2d.loops.GeneralRenderer;
+
+import java.util.GregorianCalendar;
+
 /**
  * Created by student on 3/7/2017.
  */
 public class Pet implements Boardable{
 
-    public String name;
-    public String ownerName;
-    public String color;
-    public String ownerEmail;
-    protected int gender;
+    private String name;
+    private String ownerName;
+    private String color;
+    private String ownerEmail;
+    protected String gender;
 
-    
+    //Boardable data members
+    private GregorianCalendar boardStart;
+    private GregorianCalendar boardEnd;
+
+
+    public Pet(String name, String ownerName, String ownerEmail, String color){
+        this.name = name;
+        this.ownerName = ownerName;
+        this.ownerEmail = ownerEmail;
+        this.color = color;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getOwnerName(){
+        return ownerName;
+    }
+
+    public String getColor(){
+        return color;
+    }
+
+    public void setGender(String gender){
+        this.gender = gender;
+    }
+
+    public String getGender(){
+        return gender;
+    }
+
+    public String getOwnerEmail(){
+        return ownerEmail;
+    }
+
+    public String toString(){
+        return
+                "Name: " + name +
+                ", Owner's Name: " + ownerName +
+                ", Owner's email: " + ownerEmail +
+                ", Color: " + color +
+                ", Gender: " + gender;
+    }
 
     @Override
     public void setBoardStart(int month, int day, int year) {
-
+        boardStart = new GregorianCalendar(year, month, day);
     }
 
     @Override
     public void setBoardEnd(int month, int day, int year) {
-
+        boardEnd = new GregorianCalendar(year, month, day);
     }
 
     @Override
     public boolean boarding(int month, int day, int year) {
-        return false;
+        GregorianCalendar boardingDate = new GregorianCalendar(year, month, day);
+        if(boardingDate.after(boardEnd) || boardingDate.before(boardStart)){
+            return false;
+        }
+        return true;
     }
+
+
 }
